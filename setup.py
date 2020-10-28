321ead406af515f1f879f4769c13fb0ff804214f
guarin <guarin.fluck@epfl.ch>
2020-10-28 09:59:40 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="openpifpaf_vcoco",
+    version="0.1.0",
+    packages=find_packages(include=["openpifpaf_vcoco", "openpifpaf_vcoco.*"]),
+    install_requires=[],
+    extras_require={
+    },
+)
