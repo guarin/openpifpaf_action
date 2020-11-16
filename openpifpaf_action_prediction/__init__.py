d7df2cd3a22ad51bfa60aec97c57265a7785e04d
guarin <guarin.fluck@epfl.ch>
2020-11-16 16:39:04 +0100

    Register decoder

--- openpifpaf_action_prediction/__init__.py
+++ openpifpaf_action_prediction/__init__.py
@@ -14,6 +14,7 @@ from . import utils
 # openpifpaf.network.factory module is overwritten in openpifpaf.network.__init__
 # need to import it manually
 network_factory = importlib.import_module("openpifpaf.network.factory")
+decoder_factory = importlib.import_module("openpifpaf.decoder.factory")
 
 
 def register():
@@ -21,6 +22,7 @@ def register():
     network_factory.HEAD_FACTORIES[
         headmeta.AifCenter
     ] = openpifpaf.network.heads.CompositeField3
+    decoder_factory.DECODERS.add(decoder.aif.AifCenter)
     openpifpaf.transforms.Preprocess.annotations_inverse = (
         lambda annotations, meta: annotations
     )
