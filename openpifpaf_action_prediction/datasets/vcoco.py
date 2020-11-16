828066b43cd9f3e0c199cf231aa3b2a3a36dcab8
guarin <guarin.fluck@epfl.ch>
2020-11-16 16:17:44 +0100

    Reverse partial dataset

--- openpifpaf_action_prediction/datasets/vcoco.py
+++ openpifpaf_action_prediction/datasets/vcoco.py
@@ -309,7 +309,7 @@ class Vcoco(DataModule):
         annotations = []
         images = set()
 
-        for ann in data.coco.dataset["annotations"][:20]:
+        for ann in data.coco.dataset["annotations"]:
             num_actions = sum([ann["vcoco_action_labels"][i] for i in action_indices])
             num_keypoints = ann["num_keypoints"]
             if (
