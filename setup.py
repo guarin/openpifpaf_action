03b0574f7ff92314d2be06d953fb66237f4f15be
guarin <guarin.fluck@epfl.ch>
2021-01-11 16:12:17 +0100

    Update dependencies

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="openpifpaf_action",
     version="0.1.0",
     packages=find_packages(include=["openpifpaf_action", "openpifpaf_action.*"]),
-    install_requires=["openpifpaf"],
+    install_requires=["openpifpaf", 'numpy', 'torch', 'matplotlib', 'pandas', 'scikit'],
     extras_require={"dev": ["black"]},
 )
